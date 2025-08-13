b3b882e36a8b9fef2a1d8f2ad31b8077d1c6abf6
Flor De Bock <flor.debock@yazzoom.com>
2025-08-13 19:06:18 +0200

    Minimal front page happy birthday app

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <header class="absolute inset-x-0 top-0 z-50">
+        <nav aria-label="Global" class="flex items-center justify-between p-6 lg:px-8">
+            <div class="hidden lg:flex lg:flex-1 lg:justify-end flex items-center justify-center gap-x-6">
+                <a href="#" class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign Up</a>
+                <a href="#" class="text-sm/6 font-semibold text-gray-900">Login <span aria-hidden="true">→</span></a>
+            </div>
+        </nav>
+    </header>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+  <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
+  <div class="bg-white">
+    <div class="relative isolate px-6 pt-14 lg:px-8">
+      <div aria-hidden="true" class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
+        <div style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)" class="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"></div>
+      </div>
+      <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-28">
+        <div class="text-center">
+          <h1 class="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">The Birthday App</h1>
+          <p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">Automate wishing people a happy birthday.</p>
+        </div>
+        <img src="/birthday-cake-3177675_1280.png" alt="" class="h-128 w-auto" />
+      </div>
+    </div>
+  </div>
+</template>
